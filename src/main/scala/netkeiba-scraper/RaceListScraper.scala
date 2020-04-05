aa23b92e111d820829348349ba0a07447c40a32b
cheery-doggo <rebuilding15762@gmail.com>
2020-04-05 17:10:33 +0900

    リファクタ:可視性とメソッド順序を変更

--- src/main/scala/netkeiba-scraper/RaceListScraper.scala
+++ src/main/scala/netkeiba-scraper/RaceListScraper.scala
@@ -4,35 +4,12 @@ import java.io.File
 
 import org.apache.commons.io.FileUtils
 
+/**
+  * レースの結果が載っているURLのリストを生成するための処理群。
+  * https://db.netkeiba.com/race/list/yyyymmdd/でその日付のレース結果へのリンクの一覧が記載されたページ。
+  * https://db.netkeiba.com/race/[0-9]{12}で特定のレースの結果が記載されたページ。
+  */
 object RaceListScraper {
-  def extractRaceList(baseUrl: String) = {
-    "/race/list/\\d+/".r
-      .findAllIn(io.Source.fromURL(baseUrl, "EUC-JP").mkString)
-      .toList
-      .map("http://db.netkeiba.com" + _)
-      .distinct
-  }
-
-  def extractPrevMonth(baseList: String) = {
-    "/\\?pid=[^\"]+".r
-      .findFirstIn(
-        io.Source
-          .fromURL(baseList, "EUC-JP")
-          .getLines
-          .filter(_.contains("race_calendar_rev_02.gif"))
-          .toList
-          .head)
-      .map("http://db.netkeiba.com" + _)
-      .get
-  }
-
-  def extractRace(listUrl: String) = {
-    "/race/\\d+/".r
-      .findAllIn(io.Source.fromURL(listUrl, "EUC-JP").mkString)
-      .toList
-      .map("http://db.netkeiba.com" + _)
-      .distinct
-  }
 
   def scrape(period: Int) = {
     var baseUrl = "http://db.netkeiba.com/?pid=race_top"
@@ -57,4 +34,34 @@ object RaceListScraper {
       i += 1
     }
   }
+
+  private def extractRaceList(baseUrl: String) = {
+    "/race/list/\\d+/".r
+      .findAllIn(io.Source.fromURL(baseUrl, "EUC-JP").mkString)
+      .toList
+      .map("http://db.netkeiba.com" + _)
+      .distinct
+  }
+
+  private def extractRace(listUrl: String) = {
+    "/race/\\d+/".r
+      .findAllIn(io.Source.fromURL(listUrl, "EUC-JP").mkString)
+      .toList
+      .map("http://db.netkeiba.com" + _)
+      .distinct
+  }
+
+  private def extractPrevMonth(baseList: String) = {
+    "/\\?pid=[^\"]+".r
+      .findFirstIn(
+        io.Source
+          .fromURL(baseList, "EUC-JP")
+          .getLines
+          .filter(_.contains("race_calendar_rev_02.gif"))
+          .toList
+          .head)
+      .map("http://db.netkeiba.com" + _)
+      .get
+  }
+
 }
